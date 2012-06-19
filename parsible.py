183f0f7d694ce9c37d0c995eef813a69509b73f3
Andrew Gross <andrew@yipit.com>
2012-06-19 15:25:39 -0400

    Adding simpler debugging to Parsible.
    It is now possible to debug your processors as your run them through
    Parsible.  When using '--debug True' the processors loop will no longer
    swallow all exceptions and will instead print out a traceback to stdout.
    
    In the future this will be expanded to allow individual processors to
    import the debugging logger so that more fine grainer errors can be
    caught for further analysis.
    
    By default debugging is disabled and no changes to any scripts need to
    be made to update Parsible.

--- parsible.py
+++ parsible.py
@@ -1,22 +1,24 @@
 #!/bin/python
-import sys, time, os, signal, imp, argparse
+import sys, time, os, signal, imp, argparse, logging, traceback
 
 class Parsible(object):
     def import_plugins(self):
         # Initialize our lists
         self.processors = []
-        
-        current_file = os.path.abspath(__file__)
-        current_directory = os.path.abspath(os.path.join(current_file, os.path.pardir))
-        plugins_directory = current_directory + "/plugins"
-        
+
         # Map our directory names to the prefixes on methods we want to check out.
         plugin_mappings = {
                             'parsers' : 'parse',
-                            'processors' : 'process',
-                            'outputs' : 'output'
+                            'processors' : 'process'
                           }
 
+        # Figure out where we are and start looking for plugins
+        current_file = os.path.abspath(__file__)
+        current_directory = os.path.abspath(os.path.join(current_file, os.path.pardir))
+        plugins_directory = current_directory + "/plugins"
+
+        # Iterate through our potential plugin locations so we can import everything
+        # IMPORTANT:  Without this block we can't use the buzzword 'Autodiscover', very necessary
         for plugin_type in plugin_mappings.keys():
             directory = plugins_directory + "/" + plugin_type
             for f in os.listdir(directory):
@@ -29,24 +31,37 @@ class Parsible(object):
                         # Filter down to methods that have the appropriate prefix
                         if method.startswith(plugin_mappings[plugin_type]):
                             _temp = __import__('plugins.%s.%s' % (plugin_type, parser_file), globals(), locals(), [method], -1)
+                            # Figure out what we should do with our newly discovered methods
                             if "parse" in plugin_mappings[plugin_type]:
                                 if self.parser is not None:
                                     if method == self.parser:
                                         setattr(self, "parsing_function", getattr(_temp, method))
                                 else:
-                                    # Currently this will overwrite any previously found parsing functions, NBD
+                                    # Set the first parser we find, overwrite this laster if we find one specified
                                     setattr(self, "parsing_function", getattr(_temp, method))
                             # Construct our list of processing functions that we will call on each parsed line
                             elif "process" in plugin_mappings[plugin_type]:
                                 self.processors.append(getattr(_temp, method))
 
-    def __init__(self, input_file, parser, pid_file):
-        # Some messy business to import unknown files at runtime
+    def set_logging(self):
+        logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] %(asctime)s - %(message)s')
+        logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(asctime)s - %(message)s')
+        self.logger = logging.getLogger('parsible')
+        if self.debug:
+            self.logger.setLevel(logging.DEBUG)
+        else:
+            self.logger.setLevel(logging.ERROR)
+
+
+    def __init__(self, input_file, parser, pid_file, debug):
+        self.debug = debug
+        self.set_logging()
+        # Some messy business to import unknown files at runtime, cool stuff inside
         self.parser = parser
         self.import_plugins()
         # Keep internal references to these so we can change and refresh them properly
         self.input_file = input_file
-        self.pid_file = pid_file  
+        self.pid_file = pid_file
 
     def exit(self, status):
         os.remove(self.pid_file)
@@ -65,6 +80,7 @@ class Parsible(object):
         return
 
     def set_pid_file(self):
+        # All this to set up a PID file
         f = open(self.pid_file, 'w')
         PID = str(os.getpid())
         f.write(PID)
@@ -75,15 +91,23 @@ class Parsible(object):
 
     def follow(self):
         # Shamelessly drafted from http://www.dabeaz.com/generators/Generators.pdf
+        if self.debug:
+            iterations = 0
         # Go to the end of the file
         self.log_file.seek(0,2)
         while True:
             # Get our latest line (via a Generator) or None if nothing new is in place
             line = self.log_file.readline()
             if not line:
-                # Essentially spinlock on our logfile, depending on update speed this can be lowered
+                if self.debug:
+                    iterations += 1
+                    self.logger.debug('Tick Tock, waited for {} iterations'.format(iterations))
+                # Essentially spinlock on our logfile waiting for updates to become available
+                # Depending on update speed this iteration time can be decreased
                 time.sleep(0.1)
                 continue
+            if self.debug:
+                iterations = 0
             # Yield so we can be called as a generator, decoupling the waiting issues.
             # Our parsing function can be evaluated later
             yield self.parsing_function(line)
@@ -92,8 +116,12 @@ class Parsible(object):
         for process in self.processors:
             try:
                 process(line)
-            except:
+            except Exception, e:
                 # We can add some custom logic if needed, such as counting how many lines have issues
+                # For now we just swallow errors, since the spice must flow, err parsing must continue.
+                if self.debug:
+                    self.logger.debug(e)
+                    traceback.print_exc(file=sys.stdout)
                 continue
 
     def main(self):
@@ -107,40 +135,52 @@ class Parsible(object):
         # Abstract all the messy generator logic away into a simple for-each
         for parsed_line in parsed_log_file:
             # The processors should take care of outputting data as they see fit
+            if self.debug:
+                self.logger.debug(parsed_line)
             self.run_processors(parsed_line)
-        
-        # We probably will never reach here, but it pays to be tidy just in case
+
+        # We probably will never reach here, but it pays to be tidy just in case we change code in the future
         self.log_file.close()
 
 if __name__ == '__main__':
 
+    # Just setting up command line arguments.
+    # Only thing interesting here is the defaults set for some options. You can skip this trying to get to the meat.
     cmdline = argparse.ArgumentParser(usage="usage: parsible.py --log-file /var/log/mylog [options]",
                                       description="Tail a log file and filter each line to generate metrics that can be output to any desired endpoint.")
 
-    cmdline.add_argument('--log-file', 
-                         '-l', 
+    cmdline.add_argument('--log-file',
+                         '-l',
                          action='store',
                          help='The absolute path to the log file to be parsed, Ex: /var/log/mylog',
                          dest='input_file',
                          required=True
                         )
 
-    cmdline.add_argument('--parser', 
-                         '-p', 
+    cmdline.add_argument('--parser',
+                         '-p',
                          action='store',
                          help='Name of the parsing method to use, should start with "parse_", Ex: parse_nginx',
                          dest='parser',
                          default=None
                         )
 
-    cmdline.add_argument('--pid-file', 
-                         '-f', 
+    cmdline.add_argument('--pid-file',
+                         '-f',
                          action='store',
                          help='Absolute path to use for the PID file, Ex: /tmp/parsible.pid',
                          dest='pid_file',
                          default='/tmp/parsible.pid'
                         )
 
+    cmdline.add_argument('--debug',
+                         '-d',
+                         action='store',
+                         help='Enable Debugging',
+                         dest='debug',
+                         default=False
+                        )
+
     args = cmdline.parse_args()
-    p = Parsible(args.input_file, args.parser, args.pid_file)
+    p = Parsible(args.input_file, args.parser, args.pid_file, args.debug)
     p.main()
